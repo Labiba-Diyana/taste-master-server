f6e2d848741d3aa636ee1f09b0014948b7e643c6
diyana <labibadiyana100@gmail.com>
2023-08-04 02:05:58 +0600

    finally done

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs)
-})
+});
 
 app.get('/chefs/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
     const selectedChef = chefs.find(n => n._id === id);
     res.send(selectedChef);
-})
+});
 
 app.listen(port, () => {
     console.log(`My server is running at port: ${port}`)
-})
+});
